a5d73400da0fe26780f05f57e789dc74e2d95de2
Emannil <emanil-9@student.ltu.se>
2019-11-21 13:04:25 +0100

    Add project files.

---
+++ Task1/Task1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int is_sorted(int arr[4]) {
+	bool result;
+	for (int i = 0; i < 3; i++) { //i goes through the whole array
+		if (arr[i] > arr[i + 1]) {
+			result = false;
+			return result;
+		}
+		else if (arr[i] <= arr[i + 1]) {
+			result = true;
+		}
+	}
+	return result;
+}
+
+int main() {
+
+	int array[4] = {};
+	for (int i = 0; i < 4; i++) { 
+		cout << "Enter 4 numbers for the array: ";
+		cin >> array[i];
+	}
+	int returnedValue = is_sorted(array);
+	cout << "Is the array sorted? " << returnedValue << endl;
+
+
+	return(0);
+}
+
---
+++ Task2/Task2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+bool is_palindrome(char str[], int len) {
+
+	for (int i = len - 1; i >= 0; i--) {
+		if (str[len - 1 - i] != str[i]) // Checks if the characters from oposite side are the same
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	char userinp;
+	char word[20]; //Set the word to a maximum of 20 characters
+	int lenght;
+
+	do {
+		cout << "Type a word: ";
+		cin >> word;
+
+		lenght = strlen(word);  
+		int returned_Value = is_palindrome(word, lenght);
+
+		if (returned_Value == 0)
+			cout << "Not a palindrome" << endl;
+
+		else if (returned_Value == 1)
+			cout << "palindrome" << endl;
+
+		cout << "Test again (Y/N)?" << endl;
+		cin >> userinp;
+
+	} while (userinp == 'Y' || userinp == 'y');
+
+
+	return(0);
+}
+
