a9b0f104bb470412b7c3280e78340c171efa39ed
Emannil <emanil-9@student.ltu.se>
2019-12-01 03:30:52 +0100

    Did stuff and added some stuff

--- Task7/Task7.cpp
+++ Task7/Task7.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	while (true) {
+		int inpChoice;
+		cout << "Menu :" << endl;
+		cout << "1: Task 1" << endl;
+		cout << "2: Task 2" << endl;
+		cout << "3: Task 3" << endl;
+		cout << "4: Task 4" << endl;
+		cout << "5: Task 5" << endl;
+		cout << "6: Task 6" << endl;
+		cout << "0: Exit" << endl;
+		cin >> inpChoice;
+
+		switch (inpChoice) {
+		case 1:
+			int is_sorted(int arr[4]);
+			break;
+
+		case 2:
+			bool is_palindrome(char str[], int len);
+			break;
+
+		case 3:
+			void array_rows_cols(int** arr, int userInpRows, int userInpColums);
+			break;
+
+		case 4:
+			void swap_sort(int& a, int& b, int& c, bool order);
+			break;
+
+		case 5:
+			void shrink_array(int* arr, int size);
+			break;
+
+		case 6:
+			int vector_Alternatives(int userInp, string wordInp, string userYN, vector<string> & vector);
+			break;
+
+		case 0:
+			return 0;
+
+		default:
+			cout << "No such task exist" << endl;
+			break;
+		}
+	}
+	
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- lab2lib/lab2lib.h
+++ lab2lib/lab2lib.h
@@ -1,1 +1,15 @@
 #pragma once
+
+int is_sorted(int arr[4]);
+
+bool is_palindrome(char str[], int len);
+
+void array_rows_cols(int** arr, int userInpRows, int userInpColums);
+
+void swap_sort(int& a, int& b, int& c, bool order);
+
+void shrink_array(int* arr, int size);
+
+int vector_Alternatives(int userInp, string wordInp, string userYN, vector<string> &vector);
+
+
